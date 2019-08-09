4b3c2fa8b5b9ddc66259e78ffd9fa0ca4cd07eec
bkviswam <baiju.k.viswam@gmail.com>
2019-08-09 11:47:29 -0700

    Added problem description

--- src/com/google/NonRepeatingInt.java
+++ src/com/google/NonRepeatingInt.java
@@ -1,5 +1,14 @@
 package com.google;
 
+/**
+Given an array of integers where every integer occurs three times except for one integer, which only occurs once, find and return the non-duplicated integer.
+
+For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], return 19.
+
+Do this in O(N) time and O(1) space.
+**/
+
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
